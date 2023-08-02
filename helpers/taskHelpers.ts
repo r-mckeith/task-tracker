c527da2850dafa6e85a9956581004467ebf28fd4
r-mckeith <r.mckeith@me.com>
2023-08-02 12:16:20 -0400

    optomistically update state

--- helpers/taskHelpers.ts
+++ helpers/taskHelpers.ts
@@ -11,41 +11,47 @@ export const handleDelete = async (id: number, dispatch: React.Dispatch<any>) =>
 };
 
 export const handleToggleCompleted = async (id: number, completed: boolean, tasks: TaskInterface[], dispatch: React.Dispatch<any>) => {
+  dispatch({ type: 'TOGGLE_COMPLETED', id });
+
   try {
     await toggleCompleted(id, completed, tasks);
-
-    dispatch({ type: 'TOGGLE_COMPLETED', id });
   } catch (error) {
     console.error('Failed to toggle task:', error);
+
+    dispatch({ type: 'TOGGLE_COMPLETED', id });
   }
 };
 
-export  const handleToggleScopeforDay = async (id: number, inScope: boolean, tasks: TaskInterface[], dispatch: React.Dispatch<any>) => {
+export const handleToggleScopeforDay = async (id: number, inScope: boolean, tasks: TaskInterface[], dispatch: React.Dispatch<any>) => {
+  dispatch({ type: 'TOGGLE_DAY', id: id, inScopeDay: inScope });
+
   try {
     await toggleScopeForDay(id, inScope, tasks);
+  } catch (error) {
+    console.error('Failed to toggle scope for day:', error);
 
-  dispatch({ type: 'TOGGLE_DAY', id: id, inScopeDay: inScope, });
-} catch (error) {
-  console.error('Failed to toggle scope for day:', error);
-}
+    dispatch({ type: 'TOGGLE_DAY', id: id, inScopeDay: !inScope });
+  }
 };
 
-export  const handleToggleScopeforWeek = async (id: number, inScope: boolean, tasks: TaskInterface[], dispatch: React.Dispatch<any>) => {
+export const handleToggleScopeforWeek = async (id: number, inScope: boolean, tasks: TaskInterface[], dispatch: React.Dispatch<any>) => {
+  dispatch({ type: 'TOGGLE_WEEK', id: id, inScopeWeek: inScope });
+
   try {
     await toggleScopeForWeek(id, inScope, tasks);
+  } catch (error) {
+    console.error('Failed to toggle scope for week:', error);
 
-  dispatch({ type: 'TOGGLE_WEEK', id: id, inScopeDay: inScope });
-} catch (error) {
-  console.error('Failed to toggle scope for day:', error);
-}
-}; 
+    dispatch({ type: 'TOGGLE_WEEK', id: id, inScopeWeek: !inScope });
+  }
+};
 
 export const findChildTasks = (taskId: number, tasks: TaskInterface[]): TaskInterface[] => {
   if (!tasks) {
     console.error('Tasks is undefined!');
     return [];
   }
-  
+
   const directChildren = tasks.filter(task => task.parentId === taskId);
   let allChildren = [...directChildren];
 
